c65517504dbdbc7d0628d11c4b9895644bca8cab
Mark <mark.e.eaton@gmail.com>
2021-10-27 19:08:58 -0500

    updated env var name

--- db/goldenLeopardDb.js
+++ db/goldenLeopardDb.js
@@ -2,7 +2,7 @@
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    connectionString: process.env.DB_CONNECTION_STRING,
+    connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
   },
 });
