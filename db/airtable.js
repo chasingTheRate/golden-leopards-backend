7dca7e84f714af788595e46948c76941ec5ef646
Mark <mark.e.eaton@gmail.com>
2022-03-07 11:59:22 -0600

    score

--- db/airtable.js
+++ db/airtable.js
@@ -20,7 +20,9 @@ const getSeasonSchedule = async () => {
       'start',
       'end',
       'recordedGame',
-      'veoLink'
+      'veoLink',
+      'ourScore',
+      'opponentScore'
     ]
   }).firstPage();
   return records.map(r => {
