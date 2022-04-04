addfa147f679354ed403cb9afb352065dc761b46
Mark <mark.e.eaton@gmail.com>
2022-04-04 11:33:07 -0500

    cleanup unused files

--- controllers/goldenLeopardController.js
+++ controllers/goldenLeopardController.js
@@ -1,84 +1,9 @@
-const db = require('../db/goldenLeopardDb');
 const airtableDb = require('../db/airtable');
 const scripts = require('../scripts');
 const moment = require('moment');
 const _ = require('lodash');
 const redis = require('../db/redis');
 
-const { v4: uuidv4 } = require('uuid');
-
-const getSeasons = async () => {
-  const result = await db.getSeasons();
-  return result ? result : [];
-}
-
-const createSeasons = async (seasons) => {
-  const result = await db.createSeasons(seasons);
-  return result ? result : [];
-}
-
-const deleteSeason = async (seasonId) => {
-  return db.deleteSeason(seasonId);
-}
-
-const getGames = async () => {
-  const result = await db.getGames();
-  return result ? result : [];
-}
-
-const createGames = async (games) => {
-  const result = await db.createGames(games);
-  return result ? result : [];
-}
-
-const deleteGame = async (gameId) => {
-  return db.deleteGame(gameId);
-}
-
-const getPlayers = async () => {
-  const result = await db.getPlayers();
-  return result ? result : [];
-}
-
-const createPlayers = async (players) => {
-  const result = await db.createPlayers(players);
-  return result ? result : [];
-}
-
-const deletePlayer = async (playerId) => {
-  return db.deletePlayer(playerId);
-}
-
-const getGameSegments = async () => {
-  const result = await db.getGameSegments();
-  return result ? result : [];
-}
-
-const createGameSegments = async (gameSegments) => {
-  const result = await db.createGameSegments(gameSegments);
-  return result ? result : [];
-}
-
-const deleteGameSegment = async (gameSegmentId) => {
-  return db.deleteGameSegment(gameSegmentId);
-}
-
-const getPlayersStats = async () => {
-  const result = await db.getPlayersStats();
-  const rows = result ? result.rows : [];
-  return rows ? rows : [];
-}
-
-const getTeamRecord = async () => {
-  const result = await db.getTeamRecord();
-  return result[0] ? result[0] : [];
-}
-
-const getUpcomingGames = async () => {
-  const date = new Date();
-  const result = await db.getUpcomingGames(date.toISOString());
-  return result ? result : [];
-}
 
 const getSeasonSchedule = async () => {
 
@@ -166,21 +91,6 @@ const getNextGames = async () => {
 }
 
 module.exports = {
-  getSeasons,
-  createSeasons,
-  deleteSeason,
-  getGames,
-  createGames,
-  deleteGame,
-  getPlayers,
-  createPlayers,
-  deletePlayer,
-  getGameSegments,
-  createGameSegments,
-  deleteGameSegment,
-  getPlayersStats,
-  getTeamRecord,
-  getUpcomingGames,
   getSeasonSchedule,
   getTournamentSchedules,
   updateTournament,
