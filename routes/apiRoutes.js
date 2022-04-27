fc79336480148b0bcb0815edd9872366906bc867
Mark <mark.e.eaton@gmail.com>
2022-04-27 18:39:23 -0500

    added clear cache route for tournament schedules

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,8 +5,11 @@ const glService = require('../services/goldenLeopardService');
 
 router.get('/schedules/season', glService.getSeasonSchedule);
 router.get('/schedules/nextgames', glService.getNextGames);
+
+router.get('/schedules/tournaments/clearCache', glService.clearTournamentScheduleCache);
 router.get('/schedules/tournaments', glService.getTournamentSchedules);
 router.put('/schedules/tournaments/:id', glService.updateTournament);
+
 router.put('/schedules/season/checkForUpdates', glService.checkForUpdates);
 
 router.get('/roster', glService.getRoster);
